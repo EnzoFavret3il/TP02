875b053d12c416a816adba19e8df302246c271e2
EnzoFavret3il <favrete@3il.fr>
2023-11-23 11:40:12 +0100

    Exo 2.1 presque fini

--- src/etudiant/TestEtudiant.java
+++ src/etudiant/TestEtudiant.java
@@ -4,7 +4,9 @@ public class TestEtudiant {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Etudiant Oui=new Etudiant("Dagobère");
+		Oui.travailler();
+		Oui.seReposer();
 	}
 
 }
