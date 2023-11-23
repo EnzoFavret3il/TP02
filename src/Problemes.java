f0bd8800284ca44b8d922bdfb9b6573d85b0dee4
EnzoFavret3il <favrete@3il.fr>
2023-11-23 11:59:13 +0100

    exo 3.1

---
+++ src/Problemes.java
@@ -0,0 +1,12 @@
+
+public class Problemes {
+	/*méthodes 1 et 2 void*/
+	//les deux méthodes partagent le même nom
+	
+	/*métohdes 3 et 4*/
+	//Aucun problèmes
+	
+	/*méthodes 5 et 6*/
+	//Partagent un même nom et un même argument
+	
+}
