f9ad37db4a3b9fb486b441e1f52a69e544e0ee1f
EnzoFavret3il <favrete@3il.fr>
2023-11-23 11:36:18 +0100

    Update

--- src/etudiant/Etudiant.java
+++ src/etudiant/Etudiant.java
@@ -7,20 +7,11 @@ public class Etudiant {
 		this.nom = nom;
 	}
 	public void travailler() {
-		if(this.nom=="Philippe") {
 			System.out.println(this.nom+" se met au travail !");
 		}
-		else {
-			System.out.println(this.nom+ " travail plus ou moins...");
-		}
-	}
+	
 	public void seReposer() {
-		if(this.nom=="Philippe") {
 			System.out.println(this.nom+" se repose");
-		}
-		else {
-			System.out.println(this.nom+ " se repose comme d'habitude");
-		}
 	}
 	
 }
